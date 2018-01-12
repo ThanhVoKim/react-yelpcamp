10ac551631af6340a3e6a93a0c5825565cd0131e
thanh <lycanthrope12@yahoo.com>
2018-01-12 09:31:21 +0700

    m

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyDv_p41BBFlWdFcQutYQfNFH33xf3ITnPA',
+  authDomain: 'testing-f0bde.firebaseapp.com',
+  databaseURL: 'https://testing-f0bde.firebaseio.com',
+  projectId: 'testing-f0bde',
+  storageBucket: 'testing-f0bde.appspot.com',
+  messagingSenderId: '180026380089'
 };
 firebase.initializeApp(config);
 
